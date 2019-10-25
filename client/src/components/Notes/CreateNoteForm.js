d4275033487ccd59fa336649728453cfe38a333d
CTSchulmeister <CTSchulmeister@gmail.com>
2019-10-25 19:10:10 -0400

    Change CreateNoteForm.js to use new form components

--- client/src/components/Notes/CreateNoteForm.js
+++ client/src/components/Notes/CreateNoteForm.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { createNote } from '../../actions/noteActions';
+
+import FormErrorBoundary from '../Form/FormErrorBoundary';
+import FormHeader from '../Form/FormHeader';
+import TextInput from '../Form/TextInput';
+import TextArea from '../Form/TextArea';
+import SubmitButton from '../Form/SubmitButton';
 
 class CreateNoteForm extends Component {
     constructor(props) {
@@ -13,28 +18,8 @@ class CreateNoteForm extends Component {
             body: ''
         };
 
-        this.focusInput.bind(this);
-        this.focusTextarea.bind(this);
         this.handleChange.bind(this);
         this.handleSubmit.bind(this);
-    }
-
-    focusInput = (event) => {
-        if(event.target.classList.contains('form__input-group')) {
-            event.target.querySelector('.form__text-input').focus();
-        } else if(event.target.parentElement.classList.contains('form__hint')) {
-            event.target.parentElement.parentElement.querySelector('.form__text-input').focus();
-        } else {
-            event.target.parentElement.querySelector('.form__text-input').focus();
-        }
-    }
-
-    focusTextarea = (event) => {
-        if(event.target.classList.contains('form__input-group')) {
-            event.target.querySelector('.form__text-area').focus();
-        } else {
-            event.target.parentElement.querySelector('.form__text-area').focus();
-        }
     }
 
     handleChange = (event) => {
@@ -76,38 +61,26 @@ class CreateNoteForm extends Component {
 
     render() {
         return (
-            <form className="form" onSubmit={ this.handleSubmit }>
-                <div className="form__header">
-                    <h2 className="form__title">Create Note</h2>
-                </div>
-                <div className="form__input-group" onClick={ this.focusInput }>
-                    <input
-                        className="form__text-input"
+            <FormErrorBoundary formName="Create Note">
+                <form className="form" onSubmit={ this.handleSubmit }>
+                    <FormHeader text="Create Note" />
+                    <TextInput
                         type="text"
                         name="title"
                         value={ this.state.title }
                         onChange={ this.handleChange }
+                        label="Title"
                     />
-                    <label className="form__label" htmlFor="title">Title</label>
-                </div>
-                <div className="form__input-group form__input-group--text-area" onClick={ this.focusTextarea }>
-                    <textarea
-                        className="form__text-area"
+                    <TextArea
                         name="body"
-                        onChange={ this.handleChange }
                         value={ this.state.body }
-                    >
-                    </textarea>
-                    <label className="form__label--text-area" htmlFor="body">Body</label>
-                </div>
-                <div className="form__submit-group">
-                    <input
-                        className="button button--med"
-                        type="submit"
-                        value="Create"
+                        onChange={ this.handleChange }
+                        label="Body"
                     />
-                </div>
-            </form>
+                    <SubmitButton text="Create" />
+                </form>
+            </FormErrorBoundary>
+            
         )
     }
 }
