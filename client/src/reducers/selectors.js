395e36547cc278a30b111e1ae8cf6580a3f5b1b7
CTSchulmeister <CTSchulmeister@gmail.com>
2019-11-03 19:54:13 -0500

    Create invitation state selectors

--- client/src/reducers/selectors.js
+++ client/src/reducers/selectors.js
@@ -32,4 +32,30 @@ export const selectRegistrationErrors = state => {
 
 export const selectServerErrorStatus = state => {
     return state.server.connectionError;
-}
+};
+
+// --- Invitations
+
+export const selectInvitationLoadingState = state => {
+    return state.invitations.loading;
+};
+
+export const selectInvitations = state => {
+    return state.invitations.invitations;
+};
+
+export const selectInvitationCreationError = state => {
+    return state.invitations.invitationCreationError;
+};
+
+export const selectAcceptInvitationError = state => {
+    return state.invitations.acceptInvitationError;
+};
+
+export const selectGetInvitationsError = state => {
+    return state.invitations.getInvitationsError;
+};
+
+export const selectDeleteInvitationError = state => {
+    return state.invitations.deleteInvitationError;
+};
